5d32673cba27457ab042a0532cb76bf79d4cee22
Alexander <hello@alexander.works>
2020-08-08 00:30:22 +0300

    Added app icon and make sliders workable

--- BMI_Calculator/ViewController.swift
+++ BMI_Calculator/ViewController.swift
@@ -9,12 +9,32 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var heightValue: UILabel!
+    @IBOutlet weak var weightValue: UILabel!
+    @IBOutlet weak var heightSlider: UISlider!
+    @IBOutlet weak var weightSlider: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction func heightSliderChanged(_ sender: UISlider) {
+        heightValue.text = String(format: "%.2f", sender.value)
+    }
+    
+    
+    @IBAction func weightSliderChanged(_ sender: UISlider) {
+        weightValue.text = String(format: "%.0f", sender.value)
+    }
+    
+    @IBAction func calculatePressed(_ sender: UIButton) {
+        let height = heightSlider.value
+        let weight = weightSlider.value
+        
+        let bmi = Int(weight / pow(height, 2))  // Using a "power" / pow() function
+        print (bmi)
+    }
+    
+    
 }
 
